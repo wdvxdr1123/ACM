ebc1d8412a092f0673f6b3965a315afcc6c3b7ed
wdvxdr <wdvxdr@foxmail.com>
2020-03-05 23:00:50 +0800

    1

--- Round3/B.cpp
+++ Round3/B.cpp
@@ -2,3 +2,22 @@
 //Author: wdvxdr
 //Date: 2020-03-04 15:16:11 
 
+using namespace std;
+
+const int maxn =1e2+7;
+
+int n,m,p[maxn],h[maxn],c[maxn];
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d",&n,&m);
+        for(int i=1;i<=m;i++)
+            scanf("%d%d%d",&p[i],&h[i],&c[i]);
+        
+    }
+    return 0;
+}
